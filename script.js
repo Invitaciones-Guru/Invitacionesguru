457dc88f2a8f4be095639669858e91380be0898f
Invitaciones-Guru <beylabriano@icloud.com>
2025-12-26 22:09:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 <script>
-    const audio = document.getElementById('bg-music');
-    const musicBtn = document.getElementById('musicControl');
-    const musicIcon = document.getElementById('musicIcon');
-    const musicText = document.getElementById('musicText');
 
-    musicBtn.addEventListener('click', () => {
-        if (audio.paused) {
-            audio.play();
-            // Cambia a estado de pausa
-            musicText.innerText = "PAUSE";
-            musicIcon.innerText = "II";
-            musicBtn.classList.add('is-playing');
-        } else {
-            audio.pause();
-            // Cambia a estado de play
-            musicText.innerText = "PLAY MUSIC";
-            musicIcon.innerText = "▶";
-            musicBtn.classList.remove('is-playing');
-        }
-    });
 
     // Mantener la animación de scroll AOS
     const observer = new IntersectionObserver((entries) => {
